0a12770aad690235426fd77d8b524276cbf7f168
meronSIS <167541754+meronsisay@users.noreply.github.com>
2025-07-12 14:23:27 +0300

    added readme file

---
+++ README.md
@@ -0,0 +1,22 @@
+A simple JavaScript ToDo App
+Allow users to add, edit, and delete tasks.
+
+users can
+ - Add new tasks
+     - Type in the input field and click the "Add" button to create a task
+     ![Task Added](./screenshots/Add-Task.png)  
+- delete task
+     - Click on a task to remove it from the list
+     ![Task Deleted](./screenshots/Delete-Task.png) 
+
+ - Edit existing tasks
+     - Click the "Edit" button to make a task editable
+     - The button changes to "Save" while editing
+     ![Task Edit](./screenshots/Edit-Task.png) 
+ 
+
+how to run The App
+ - clone this repository
+      -git clone https://github.com/meronsisay/ToDo-app-js.git
+ - open index.html
+
--- script.js
+++ script.js
@@ -6,30 +6,42 @@ function add_tasks(e) {
 
   let tasks = document.createElement("li");
   let addInput = document.querySelector(".add_task input");
-  // let editbtn = document.createElement("button")
-  // editbtn.textContent = "EDIT"
+  let editbtn = document.createElement("button");
+  editbtn.textContent = "EDIT";
 
   if (!addInput.value) return;
   // add a task from the input
   tasks.textContent = addInput.value;
   tasks.classList.add("task-card");
+  editbtn.classList.add("edit")
+
 
   // edit task
-  // tasks.addEventListener("dblclick", () => {
-  //   tasks.contentEditable = true;
-  //   tasks.focus();
-  //   tasks.style.backgroundColor = "rgb(227, 217, 238)";
-  // });
+  editbtn.addEventListener("click", () => {
+    if(tasks.isContentEditable){
+       tasks.contentEditable = false;
+       tasks.style.backgroundColor = "";
+       editbtn.textContent = "Edit";
+    }
+    else{
+       tasks.contentEditable = true;
+       tasks.focus();
+       tasks.style.backgroundColor = "rgb(227, 217, 238)";
+       editbtn.textContent = "Save"
+    }
+   
+  });
 
- // delete task
+  // delete task
   tasks.addEventListener("click", () => {
     if (!tasks.isContentEditable) {
       tasks.remove();
+      editbtn.remove();
     }
   });
 
   taskContainer.append(tasks);
-  // taskContainer.append(editbtn)
+  taskContainer.append(editbtn);
   addInput.value = "";
 }
 
